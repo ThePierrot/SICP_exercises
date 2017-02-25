6c37705f5473a10202e12d1ad5af1acd010e035d
ThePierrot <755030589@qq.com>
2017-02-25 17:01:16 -0500

    reset /2

--- 2/2-1.scm
+++
@@ -1,8 +0,0 @@
-(define (make-rat n d neg?)
-  (let ((g (gcd n d)))
-    (if (= neg? 0)
-        (cons (/ n g) (/ d g))
-        (cons (/ (- 0 n) g) (/ d g)))))
-
-(make-rat 1 2 1)
-
--- 2/2-17.scm
+++
@@ -1,7 +0,0 @@
-(define (last-pair list)
-  (if (null? (cdr list)) 
-      list
-      (last-pair (cdr list))))
-
-;test
-(last-pair (list 1 2 3 4))
