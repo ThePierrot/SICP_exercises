eecf980f27c8f43b63806e56af5f5ec251658bba
ThePierrot <755030589@qq.com>
2017-03-02 22:22:56 -0500

    finish 2.29-b

--- 2/2-29.scm
+++ 2/2-29.scm
@@ -15,4 +15,26 @@
   (car (cdr branch))) 
   
 (define (structure-is-mobile? structure) 
-  (pair? structure)) 
+  (pair? structure))
+
+(define (branch-weight branch) 
+  (let ((s (branch-structure branch))) 
+    (if (structure-is-mobile? s) 
+        (total-weight s) 
+        s))) 
+  
+(define (total-weight mobile) 
+  (+ (branch-weight (left-branch mobile)) 
+     (branch-weight (right-branch mobile))))
+
+;tests
+(define level-1-mobile (make-mobile (make-branch 2 1) 
+                                    (make-branch 1 2))) 
+(define level-2-mobile (make-mobile (make-branch 3 level-1-mobile) 
+                                    (make-branch 9 1))) 
+(define level-3-mobile (make-mobile (make-branch 4 level-2-mobile) 
+                                    (make-branch 8 2))) 
+  
+(total-weight level-1-mobile) 
+(total-weight level-2-mobile) 
+(total-weight level-3-mobile) 
