20bbf305228aff0641f3e1ebb12ef919918636f6
ThePierrot <755030589@qq.com>
2017-03-03 00:14:51 -0500

    finish 2.29

--- 2/2-29.scm
+++ 2/2-29.scm
@@ -10,7 +10,8 @@
   (car (cdr mobile))) 
   
 (define (branch-length branch) 
-  (car branch)) 
+  (car branch))
+
 (define (branch-structure branch) 
   (car (cdr branch))) 
   
@@ -27,14 +28,32 @@
   (+ (branch-weight (left-branch mobile)) 
      (branch-weight (right-branch mobile))))
 
+(define (balanced? mobile) (and (equal-torque? mobile)
+                                (if (list? (branch-structure (left-branch mobile)))
+                                    (balanced? (branch-structure (left-branch mobile)))
+                                    #t)
+                                (if (list? (branch-structure (right-branch mobile)))
+                                    (branch-structure (cdr (right-branch mobile)))
+                                    #t)))
+
+(define (equal-torque? mobile)
+  (= (* (branch-weight (left-branch mobile))
+        (car (left-branch mobile)))
+     (* (branch-weight (right-branch mobile))
+        (car (right-branch mobile)))))
+
 ;tests
-(define level-1-mobile (make-mobile (make-branch 2 1) 
-                                    (make-branch 1 2))) 
-(define level-2-mobile (make-mobile (make-branch 3 level-1-mobile) 
-                                    (make-branch 9 1))) 
-(define level-3-mobile (make-mobile (make-branch 4 level-2-mobile) 
-                                    (make-branch 8 2))) 
+(define l1-mobile (make-mobile (make-branch 2 3) 
+                               (make-branch 3 2))) 
+(define l2-mobile (make-mobile (make-branch 3 l1-mobile) 
+                               (make-branch 5 3))) 
+(define l3-mobile (make-mobile (make-branch 4 l2-mobile) 
+                               (make-branch 8 2))) 
   
-(total-weight level-1-mobile) 
-(total-weight level-2-mobile) 
-(total-weight level-3-mobile) 
+(total-weight l1-mobile) 
+(total-weight l2-mobile) 
+(total-weight l3-mobile)
+
+(balanced? l1-mobile)
+(balanced? l2-mobile)
+(balanced? l3-mobile)
