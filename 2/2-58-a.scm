9c07efed02d02139611c9870c27ed67cfcba4f1b
ThePierrot <755030589@qq.com>
2017-03-12 23:37:33 -0400

    finish 2.58-a

--- 2/2-58-a.scm
+++ 2/2-58-a.scm
@@ -1,53 +1,41 @@
-; lib
 (define (variable? x) (symbol? x))
 
 (define (same-variable? v1 v2)
   (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
 
 (define (make-sum a1 a2)
   (cond ((=number? a1 0) a2)
         ((=number? a2 0) a1)
         ((and (number? a1) (number? a2))
          (+ a1 a2))
-        (else (list '+ a1 a2))))
-
-(define (=number? exp num)
-  (and (number? exp) (= exp num)))
+        (else (list a1 '+ a2))))
 
 (define (make-product m1 m2)
   (cond ((or (=number? m1 0) (=number? m2 0)) 0)
         ((=number? m1 1) m2)
         ((=number? m2 1) m1)
         ((and (number? m1) (number? m2)) (* m1 m2))
-        (else (list '* m1 m2))))
+        (else (list m1 '* m2))))
 
-(define (sum? x) (and (pair? x) (eq? (car x) '+)))
+(define (sum? x) (if (pair? x)
+                     (eq? (car (cdr x)) '+)
+                     #f))
 
-(define (addend s) (car (cdr s)))
+(define (addend s) (car s))
 
 (define (augend s) (car (cdr (cdr s))))
 
-(define (product? x) (and (pair? x) (eq? (car x) '*)))
+(define (product? x) (if (pair? x)
+                         (eq? (car (cdr x)) '*)
+                         #f))
 
-(define (multiplier p) (car (cdr p)))
+(define (multiplier p) (car p))
 
 (define (multiplicand p) (car (cdr (cdr p))))
 
-; implementation
-(define (exponentiation? x)
-  (and (pair? x) (eq? (car x) '**)))
-
-(define (base x) (car (cdr x)))
-
-(define (exponent x) (car (cdr (cdr x))))
-
-(define (make-exponentiation base exponent)
-  (cond ((not (number? exponent)) (list '** base exponent))
-        ((= exponent 0) 1)
-        ((= exponent 1) base)
-        (else (list '** base exponent))))
-
-; main procedure
 (define (deriv exp var)
   (cond ((number? exp) 0)
         ((variable? exp) (if (same-variable? exp var) 1 0))
@@ -66,6 +54,4 @@
         (else #f)))
 
 ; test
-(deriv '(** x 3) 'x)
-(deriv '(** x y) 'x)
-(addend (make-sum 'x 'y))
+(deriv '((x * x) * 3) 'x)
