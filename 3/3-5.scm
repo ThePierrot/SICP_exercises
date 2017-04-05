0404ed06dbe1ad16c4717a919ff8f520613452b7
ThePierrot <755030589@qq.com>
2017-04-04 23:07:43 -0400

    finish 3.5

--- 3/3-5.scm
+++ 3/3-5.scm
@@ -1,3 +1,39 @@
 #lang racket
 ;; implmented by racket instead of Scheme
-(random 1 10)
+(define (square x) (* x x))
+
+(define (monte-carlo trials experiment)
+  (define (iter trials-remaining trials-passed)
+    (cond [(= trials-remaining 0)
+           (/ trials-passed trials)]
+          [(experiment)
+           (iter (- trials-remaining 1)
+                 (+ trials-passed 1))]
+          [else
+           (iter (- trials-remaining 1)
+                 trials-passed)]))
+  (iter trials 0))
+
+(define (rand-neg-1-to-1)
+  (/ (- (random 1 1000001) 500001) 500000))
+
+(define (in-unit-circle? x y)
+    (<= (sqrt (+ (square x)
+                 (square y)))
+        1))
+
+(define (pi-experiment)
+  (in-unit-circle? (rand-neg-1-to-1)
+              (rand-neg-1-to-1)))
+
+(define (estimate-integral P x1 x2 y1 y2 trials)
+  (define total-area
+    (* (- x2 x1) (- y2 y1)))
+  (* total-area (monte-carlo trials P)))
+
+;; test
+(display (estimate-integral
+          pi-experiment -1 1 -1 1 100000))
+
+
+
