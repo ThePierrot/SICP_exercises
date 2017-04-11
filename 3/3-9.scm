721bbab10c15ff45a9b9066268bacba77a6e6379
ThePierrot <755030589@qq.com>
2017-04-10 20:08:48 -0400

    finish 3.9

---
+++ 3/3-9.scm
@@ -0,0 +1,5 @@
+;; 1: Global: factorial
+;;    factorial: parameter n; expression (if (= n 1) 1 (* n (factorial (- n 1))))
+;; 2: Global: factorial fact-iter
+;;    fact-iter: product, counter, max-counter; expression
+;;    factorial : n; expression
