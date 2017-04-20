4fda1d84da7edb7c51fb522cecb17fde58244c9d
ThePierrot <755030589@qq.com>
2017-04-19 22:29:16 -0400

    finish 3.17

---
+++ 3/3-17.scm
@@ -0,0 +1,21 @@
+(define (count-pairs x) 
+  (let ((counted '())) 
+    (define (uncounted? x) 
+      (if (memq x counted) 
+          0 
+          (begin 
+            (set! counted (cons x counted)) 
+            1))) 
+    (define (count x) 
+      (if (not (pair? x)) 
+          0 
+          (+ (count (car x)) 
+             (count (cdr x)) 
+             (uncounted? x)))) 
+    (count x))) 
+
+(define third (cons 'a 'b)) 
+(define second (cons third third)) 
+(define first (cons second second)) 
+
+(count-pairs first)
